5171509a1426fa9b5d0410978300b4d3a71d1ed2
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-04-14 18:15:37 +0200

    debug installs

--- linux/setup_pyenv.sh
+++ linux/setup_pyenv.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 
-sudo apt install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt update
+sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
--- linux/setup_zsh.sh
+++ linux/setup_zsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt install zsh
-RUNZSH='no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+RUNZSH='no' sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"

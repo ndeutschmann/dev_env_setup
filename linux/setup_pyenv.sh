51a46258bd65bbb220b6148fc2a6add603bed8f3
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-04-14 18:35:48 +0200

    pyenv removed install

--- linux/setup_pyenv.sh
+++ linux/setup_pyenv.sh
@@ -9,8 +9,3 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
-
-exec zsh
-
-pyenv install 3.8.9
-pyenv global 3.8.9

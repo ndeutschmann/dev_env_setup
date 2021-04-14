fa963c7d9477ed94819039cbe052f03a3a0e0e6d
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-04-14 18:28:02 +0200

    pyenv path update correction

--- linux/setup_pyenv.sh
+++ linux/setup_pyenv.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt update
-sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
@@ -10,7 +10,7 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
-source ~/.zshrc
+exec zsh
 
 pyenv install 3.8.9
 pyenv global 3.8.9

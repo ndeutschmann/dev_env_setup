64a040366623210cbef632d0c03def2c4abb1c37
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-04-14 17:13:05 +0200

    first commit: basic linux setup

---
+++ linux/setup_pyenv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt-get update; apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+pyenv install 3.8.9
+pyenv global 3.8.9
---
+++ linux/setup_screen.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "shell '/usr/bin/zsh'" >> ~/.screenrc
+echo "escape ^Jj" >> ~/.screenrc

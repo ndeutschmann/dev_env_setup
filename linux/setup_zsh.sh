33fa56ca9645c2f8031db13b9bbf12fca72a888e
Nicolas Deutschmann <nicolas.deutschmann@gmail.com>
2021-04-14 17:40:13 +0200

    debugging zsh setup

--- linux/setup_zsh.sh
+++ linux/setup_zsh.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-apt install zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo apt install zsh
+RUNZSH='no sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cp $(echo $DIR)/.p10k.zsh ~/
+cp $(echo $DIR)/.zshrc ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
